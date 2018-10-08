51c21dfbc512795e97988bcd20a750254728a87b
pvakil <pratik.vakil@talentica.com>
2018-10-08 11:58:28 +0530

    build push commit

---
+++ build/precache-manifest.a356abb8f220c7544cae7d0a94bbedc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "549e8f8633daf8d4bb2e",
+    "url": "/static/js/main.549e8f86.chunk.js"
+  },
+  {
+    "revision": "65ea83ed07d4057d3316",
+    "url": "/static/js/1.65ea83ed.chunk.js"
+  },
+  {
+    "revision": "549e8f8633daf8d4bb2e",
+    "url": "/static/css/main.352fff88.chunk.css"
+  },
+  {
+    "revision": "65ea83ed07d4057d3316",
+    "url": "/static/css/1.bb41fc8a.chunk.css"
+  },
+  {
+    "revision": "3f4a79e30586ff3fc237fd5cf0170217",
+    "url": "/index.html"
+  }
+];
